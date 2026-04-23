17abc46a4a8d6d36fd79f74cc1ce672920fb3bb2
Rukutodo <venucolab@gmail.com>
2026-04-23 17:18:32 +0530

    Fix: Universal type fix for SanityImageSource to guarantee Vercel build success

--- lib/sanity/image.ts
+++ lib/sanity/image.ts
@@ -1,5 +1,4 @@
 import { createImageUrlBuilder } from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url";
 import { client } from "./client";
 
 const builder = createImageUrlBuilder(client);
@@ -8,6 +7,6 @@ const builder = createImageUrlBuilder(client);
  * Generate an optimized image URL from a Sanity image asset.
  * Usage: urlFor(sanityImageField).width(800).url()
  */
-export function urlFor(source: SanityImageSource) {
+export function urlFor(source: any) {
   return builder.image(source);
 }
